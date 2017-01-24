a52eb4197a8abc25c056646a653992bd8b86b7bf
yoyo3 <yao631029659@126.com>
2017-01-25 07:32:11 +0800

    init

--- addpermission.py
+++ addpermission.py
@@ -1,6 +1,7 @@
 from webapp.models import PermissionModel,RoleModel,role_permission,db
 from webapp.extensions import permissions
 from webapp import app
+from pprint import pprint
 
 with app.app_context():
     # Extensions like Flask-SQLAlchemy now know what the "current" app
@@ -16,10 +17,12 @@ with app.app_context():
             db.session.commit()
 
 
-    role = RoleModel.query.first()  # 获取第一条记录
+    role = RoleModel.query.first()  # 随便获取第一记录
     # 遍历那些没有删除的用户
     for p in PermissionModel.query.filter_by(is_delete=False):
         print('这句被执行')
+        info={'role.id':role.id,"role.is_delete":RoleModel.is_delete,"permissmodel":p.id,"PermissionModel.is_delete":PermissionModel.is_delete,"role_permission.c.is_delete": role_permission.c.is_delete}
+        pprint(info)
         r = db.session.query(role_permission).join(RoleModel).join(PermissionModel).\
             filter(
                 RoleModel.id == role.id,
